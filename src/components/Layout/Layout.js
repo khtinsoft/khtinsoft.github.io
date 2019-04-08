99699a957933faa8fcb6448c4ca24bd367cfce71
Ted <ted@gelato.im>
2019-04-09 04:57:39 +0900

    네이버 사이트 코드 추가

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,6 +8,7 @@ const Layout = ({ children, title, description }) => (
       <html lang="en" />
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="naver-site-verification" content="afc2fad93034344208fd7c386af1f9cde4eb15d5" />
     </Helmet>
     {children}
   </div>
